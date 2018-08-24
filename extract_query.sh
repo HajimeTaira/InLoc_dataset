a8fffeace477577f56b77b6a6ef1cce9a34410f1
HajimeTaira <htaira@ok.ctrl.titech.ac.jp>
2018-08-24 14:25:55 +0900

    HT: query path corrected.

--- extract_query.sh
+++ extract_query.sh
@@ -1,6 +1,6 @@
 src="query/iphone7.tar.gz"
 dst="query"
-query_url="http://only.oknet/INLOC_tmp/materials/iphone7.tar.gz"
+query_url="http://www.ok.sc.e.titech.ac.jp/INLOC/materials/iphone7.tar.gz"
 
 wget -O $src $query_url
 
