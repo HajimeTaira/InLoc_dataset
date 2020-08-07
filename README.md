81c1a43b451df4f194ca1d2801649a6fd352f205
HajimeTaira <htaira@ok.ctrl.titech.ac.jp>
2020-08-07 11:06:40 +0900

    Add new links

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ We also provide color RGB perspective images and depth maps which are generated 
 
 This repository provides the tools for constructing the whole of InLoc datset in the proper format to be easily used by [our InLoc demo and evaluation toolkit](https://github.com/HajimeTaira/WUSTL_demo). 
 
+- 2020.8.6 Update: Now we prepared alternative Dropbox links to our dataset. Please try `extract_db_all_mirror.sh` or `extract_query_mirror.sh` if the original shell scripts take much time for getting data. 
+
 ## Usage
 
-1. Download transformation matrices from <http://cvpr17.wijmans.xyz/> in the zip format. Put all zip files to ``database/alignments/zips/``. 
+1. Download and extract all database files: Run ``./extract_db_all.sh`` in the terminal. 
 
-2. Download and extract all database files: Run ``./extract_db_all.sh`` in the terminal. 
-
-3. Download and extract query images: Run ``extract_query.sh`` in the terminal. 
+2. Download and extract query images: Run ``extract_query.sh`` in the terminal. 
 
 ## Structure
 
---
+++ extract_db_all_mirror.sh
@@ -0,0 +1,58 @@
+
+# a. cutouts
+cutouts_url="https://www.dropbox.com/s/i19j9mnrrvbpywp/DUC1.tar.bz2?dl=1"
+wget -O database/cutouts/DUC1.tar.bz2 $cutouts_url 
+tar -jxvf database/cutouts/DUC1.tar.bz2 -C database/cutouts/
+
+cutouts_url="https://www.dropbox.com/s/t2izj3dkzk81jg4/DUC2.tar.bz2?dl=1"
+wget -O database/cutouts/DUC2.tar.bz2 $cutouts_url 
+tar -jxvf database/cutouts/DUC2.tar.bz2 -C database/cutouts/
+
+cutouts_url="https://www.dropbox.com/s/o9ifb0mfe57e9w7/CSE3.tar.bz2?dl=1"
+wget -O database/cutouts/CSE3.tar.bz2 $cutouts_url 
+tar -jxvf database/cutouts/CSE3.tar.bz2 -C database/cutouts/
+
+cutouts_url="https://www.dropbox.com/s/ma3t1kcr95pgoip/CSE4.tar.bz2?dl=1"
+wget -O database/cutouts/CSE4.tar.bz2 $cutouts_url 
+tar -jxvf database/cutouts/CSE4.tar.bz2 -C database/cutouts/
+
+cutouts_url="https://www.dropbox.com/s/viw9d2z3d90rfkz/CSE5.tar.bz2?dl=1"
+wget -O database/cutouts/CSE5.tar.bz2 $cutouts_url 
+tar -jxvf database/cutouts/CSE5.tar.bz2 -C database/cutouts/
+
+# b. scans
+scans_url="https://www.dropbox.com/s/8xgyswufmeqelz3/DUC1.tar.bz2?dl=1"
+wget -O database/scans/DUC1.tar.bz2 $scans_url 
+tar -jxvf database/DUC1.tar.bz2 -C database/scans/
+
+scans_url="https://www.dropbox.com/s/917yljys8vaauwq/DUC2.tar.bz2?dl=1"
+wget -O database/scans/DUC2.tar.bz2 $scans_url 
+tar -jxvf database/DUC2.tar.bz2 -C database/scans/
+
+scans_url="https://www.dropbox.com/s/e6ito8hzyerkjqy/CSE3.tar.bz2?dl=1"
+wget -O database/scans/CSE3.tar.bz2 $scans_url 
+tar -jxvf database/CSE3.tar.bz2 -C database/scans/
+
+scans_url="https://www.dropbox.com/s/2482c4wnlf68gbh/CSE4.tar.bz2?dl=1"
+wget -O database/scans/CSE4.tar.bz2 $scans_url 
+tar -jxvf database/CSE4.tar.bz2 -C database/scans/
+
+scans_url="https://www.dropbox.com/s/loulws6o8hmmsd3/CSE5.tar.bz2?dl=1"
+wget -O database/scans/CSE5.tar.bz2 $scans_url 
+tar -jxvf database/CSE5.tar.bz2 -C database/scans/
+
+# c. alignments
+src="database/alignments/zips"
+dst="database/alignments"
+
+wget -O $src/DUC1.zip "https://www.dropbox.com/s/djf0lvetc9nnx25/DUC1.zip?dl=1"
+wget -O $src/DUC2.zip "https://www.dropbox.com/s/ywvyg0zldghyzkf/DUC2.zip?dl=1"
+wget -O $src/CSE3.zip "https://www.dropbox.com/s/zx7927pe3fdbmvt/CSE3.zip?dl=1"
+wget -O $src/CSE4.zip "https://www.dropbox.com/s/nuuykyyarvdynjp/CSE4.zip?dl=1"
+wget -O $src/CSE5.zip "https://www.dropbox.com/s/jnojxclse7regf4/CSE5.zip?dl=1"
+
+for this_zip in `ls $src/*.zip`
+do
+this_dir=`basename ${this_zip%.zip}`
+unzip -d $dst"/"$this_dir $this_zip
+done
